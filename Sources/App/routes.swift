83d08358f00c9d2668664c87b4114226e7c56cc2
Isao Kono (isaoeka) <isaoeka@gmail.com>
2019-11-11 13:58:30 +0900

    :recycle: Add debug for rooting

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,6 +12,14 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    router.get("debug") { req in
+        return """
+        \(req.description)
+        \n\n---------------\n\n
+        \(req.config.description)
+        """
+    }
+
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
